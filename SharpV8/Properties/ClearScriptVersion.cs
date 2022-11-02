9b2c6e00a5c523c41c2bd52c3287ca05e72f542b
TechPizza <23627133+TechnologicalPizza@users.noreply.github.com>
2022-11-02 11:56:58 +0100

    Added main project and tests

---
+++ SharpV8/Properties/ClearScriptVersion.cs
@@ -0,0 +1,20 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCopyright("(c) Microsoft Corporation")]
+[assembly: InternalsVisibleTo("SharpV8Test")]
+
+[assembly: ComVisible(false)]
+
+namespace Microsoft.ClearScript.Properties
+{
+    internal static class ClearScriptVersion
+    {
+        public const string Triad = "7.3.4";
+        public const string Informational = "7.3.4";
+    }
+}
